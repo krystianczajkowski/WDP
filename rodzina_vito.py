fb0fcf7890068e0ec0c7d3d9946733d96b42c378
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2023-11-29 16:27:01 +0100

    Create rodzina_vito.py

---
+++ rodzina_vito.py
@@ -0,0 +1,9 @@
+
+domy = [1, 2, 4, 5, 7, 10]
+def dystans_miedzy_punktami(pkt: list):
+    odleglosci = {}
+    for e in pkt:
+        if e not in odleglosci:
+            for f in pkt:
+                if e - f != 0:
+                    odleglosci.append(abs(e-f))
