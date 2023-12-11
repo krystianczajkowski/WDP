2a7ac6d26c49ced65ad662ed9a2498746a63083c
Krystian Czajkowski <KrystianCzajkowski01@gmail.com>
2023-12-11 15:13:16 +0100

    add comments and make the code work

--- rodzina_vito.py
+++ rodzina_vito.py
@@ -1,10 +1,37 @@
 
-domy = [1, 2, 4, 5, 7, 10]
 def dystans_miedzy_punktami(pkt: list):
+    """Calculate sum of distances for each point."""
     odleglosci = {}
-    for e in pkt:
-        if e not in odleglosci:
-            for f in pkt:
-                if e - f != 0:
-                    odleglosci.append(abs(e-f))
-    return round(sum(odleglosci)/len(oldeglosci))
+    for i, e in enumerate(pkt):
+        tmp_pkt = pkt[:i] + pkt[i+1:]
+        for f in tmp_pkt:
+            if e not in odleglosci:
+                odleglosci[e] = abs(e-f)
+            else:
+                odleglosci[e] += abs(e-f)
+    return get_min_value(odleglosci)
+
+
+def get_min_value(dict):
+    """Returns smallest value from a dict.
+    
+    If two keys have the same value return only the first one.
+    """
+    min_v, min_k = 2e32, None
+    for key in dict:
+        value = dict[key]
+        if value < min_v:
+            min_v, min_k = value, key
+            
+    return min_k, min_v
+
+
+def main():
+    domy = [1, 2, 4, 5, 7, 10]
+    domy_ = domy[::-1]
+    print(dystans_miedzy_punktami(domy))
+    print(dystans_miedzy_punktami(domy_))
+
+
+if __name__ == "__main__":
+    main()
