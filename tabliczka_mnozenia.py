856680f192f1bb6ad6644d7cc2469d3b1979ee85
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2024-01-10 15:30:26 +0100

    Update tabliczka_mnozenia.py

--- tabliczka_mnozenia.py
+++ tabliczka_mnozenia.py
@@ -10,6 +10,20 @@ def tabliczka_mnożenia():
             print(str(j*i).rjust(2),end=' ')
         print()
 
+def tabliczka():
+    wynik = '*'
+    for i in range(1, 11):
+        if len(str(i)) == 2:
+            wynik += '\t ' + str(i)
+        else:
+            wynik += '\t  ' + str(i)
+
+    print(wynik)
+    for i in range(1, 11):
+        print(i, end='\t')
+        for j in range(1, 11):
+            print(str(j*i).rjust(3), end='\t')
+        print()
 
 if __name__ == "__main__":
     main()
